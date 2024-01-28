bc1b099942ce77f62e8970019e318b69050bcbda
ravalison <ravalisonjeremie@gmail.com>
2024-01-28 21:24:21 +0300

    Sary

--- src/composants/BackOffice.jsx
+++ src/composants/BackOffice.jsx
@@ -14,14 +14,15 @@ import Option from "./Option.jsx";
 import DetailAnnonce from "./enfants/DetailAnnonce.jsx";
 import Dashboard from "./Dashboard.jsx";
 import './backoffice.css';
+import { useEffect } from 'react';
 function BackOffice({ ip }) {
-  // useEffect(() => {
+  useEffect(() => {
     const dashboardPanel = document.querySelector('.dashboard-panel');
     const closeMenu = document.querySelector('.close-menu');
     closeMenu.addEventListener('click', () => {
       dashboardPanel.classList.remove('is-active');
     });
-  // });
+  });
   return (
     <>
       <div className="dashboard is-full-height">
