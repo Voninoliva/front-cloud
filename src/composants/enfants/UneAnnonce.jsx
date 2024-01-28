249062d64be4b2f68e8795c861d4d45b27e6ac3f
ravalison <ravalisonjeremie@gmail.com>
2024-01-28 22:12:56 +0300

    Sary

--- src/composants/enfants/UneAnnonce.jsx
+++ src/composants/enfants/UneAnnonce.jsx
@@ -2,34 +2,37 @@ import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
 import { useFetchData } from "../../api-integrations/getFromApi";
 import '../../assets/css/style.css';
 import 'bulma-list/css/bulma-list.css';
+import { useEffect } from 'react';
 function UneAnnonce(props) {
-    console.log(props.details);
-    const apiv = `${props.ip}/voiture/${props.details.idvoiture}`;
-    var apim = '';
-    const voitures = useFetchData(apiv);
-    if (voitures) {
-        console.log("voiture lien = ", apiv);
-        console.log("voiture json = ", voitures);
-        const v = `${props.ip}/modele/${voitures.idmodele}`;
-        apim = useFetchData(v);
-        console.log("modele an le voiture = ", apim);
-    }
-    const pour_detail = {
-        "annonce" :props.details,
-        "voiture": voitures,
-        "modele":apim,
-        "api":props.ip
-    }
-    bulmaCarousel.attach('.carousel', {
-        slidesToScroll: 1,
-        slidesToShow: 1,
-        navigation: false,
-    });
-    // Eto le izy
-    const displayedImages = [voitures.photos];
-    console.log('tyyy');
-    console.log(voitures.photos);
-    console.log('tyyy');
+    useEffect(() => {
+        console.log(props.details);
+        const apiv = `${props.ip}/voiture/${props.details.idvoiture}`;
+        var apim = '';
+        const voitures = useFetchData(apiv);
+        if (voitures) {
+            console.log("voiture lien = ", apiv);
+            console.log("voiture json = ", voitures);
+            const v = `${props.ip}/modele/${voitures.idmodele}`;
+            apim = useFetchData(v);
+            console.log("modele an le voiture = ", apim);
+        }
+        const pour_detail = {
+            "annonce": props.details,
+            "voiture": voitures,
+            "modele": apim,
+            "api": props.ip
+        }
+        bulmaCarousel.attach('.carousel', {
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            navigation: false,
+        });
+        // Eto le izy
+        const displayedImages = [voitures.photos];
+        console.log('tyyy');
+        console.log(voitures.photos);
+        console.log('tyyy');
+    })
     // displayedImages=voitures.photos;
     return (
         <>
