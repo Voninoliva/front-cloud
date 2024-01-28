50dc3562bec81c202381f8fc4f82a9e964212d5e
ravalison <ravalisonjeremie@gmail.com>
2024-01-28 20:49:58 +0300

    responsive

--- src/composants/Menu.jsx
+++ src/composants/Menu.jsx
@@ -2,12 +2,12 @@ import '@creativebulma/bulma-collapsible/dist/css/bulma-collapsible.min.css';
 import { bulmaCollapsible } from '@creativebulma/bulma-collapsible';
 import logo from '../assets/img/logo.png';
 export default function Menu() {
-    const dashboardPanel = document.querySelector('.dashboard-panel');
-    console.log(dashboardPanel + " tyyy ");
-    const closeMenu = document.querySelector('.close-menu');
-    closeMenu.addEventListener('click', () => {
-        dashboardPanel.classList.remove('is-active');
-    });
+    // const dashboardPanel = document.querySelector('.dashboard-panel');
+    // console.log(dashboardPanel + " tyyy ");
+    // const closeMenu = document.querySelector('.close-menu');
+    // closeMenu.addEventListener('click', () => {
+    //     dashboardPanel.classList.remove('is-active');
+    // });
     // bulmaCollapsible.attach('.is-collapsible');
     // document.addEventListener('load', function () {
     //     bulmaCollapsible.attach('.is-collapsible');
